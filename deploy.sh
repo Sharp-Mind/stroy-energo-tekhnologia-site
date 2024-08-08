9d390ba24472300d937888949cf873b177c5186a
Pavel Pletnev <64483275+Sharp-Mind@users.noreply.github.com>
2024-08-08 14:03:12 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r build/* portalset@134.0.116.32:/var/www/setinform.ru/
+scp -r build/* portalset@95.163.222.112:/var/www/setinform.ru/
 
 echo "Done!"
